6bb4a2dc0ea300ad7b000dd561bb170e077f811e
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2016-04-09 20:29:41 +0200

    Date formatting in UI

---
+++ gui/src/main/java/pl/pamsoft/imapcloud/formatters/CellDateFormatter.java
@@ -0,0 +1,32 @@
+package pl.pamsoft.imapcloud.formatters;
+
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CellDateFormatter<S> implements Callback<TableColumn<S, Long>, TableCell<S, Long>> {
+	private static final ThreadLocal<SimpleDateFormat> SDF = new ThreadLocal<SimpleDateFormat>() {
+		@Override
+		protected SimpleDateFormat initialValue() {
+			return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		}
+	};
+
+	@Override
+	public TableCell<S, Long> call(TableColumn<S, Long> param) {
+		return new TableCell<S, Long>() {
+			@Override
+			protected void updateItem(Long item, boolean empty) {
+				if (null == item) {
+					setText("");
+				} else {
+					SimpleDateFormat simpleDateFormat = SDF.get();
+					setText(simpleDateFormat.format(new Date(item)));
+				}
+			}
+		};
+	}
+}
