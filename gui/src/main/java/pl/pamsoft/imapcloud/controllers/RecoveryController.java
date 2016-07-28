b78a70b3f663eaebc5c389ea9721d23ef17f146b
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2016-07-28 19:20:14 +0200

    Recoveries UI uopdate

--- gui/src/main/java/pl/pamsoft/imapcloud/controllers/RecoveryController.java
+++ gui/src/main/java/pl/pamsoft/imapcloud/controllers/RecoveryController.java
@@ -68,7 +68,7 @@ public class RecoveryController implements Initializable, Refreshable {
 	};
 
 	private ChangeListener<String> availableRecoveriesComboChangeListener = (observable, oldValue, newValue) -> {
-		if (newValue != oldValue) {
+		if (null != newValue && newValue != oldValue) {
 			System.out.println("Changed to: " + newValue);
 			List<RecoveredFileDto> data = recoveriesData.get(newValue);
 			fileList.setItems(FXCollections.observableArrayList(data));
@@ -99,4 +99,7 @@ public class RecoveryController implements Initializable, Refreshable {
 		recoveryRestClient.startAccountRecovery(selectedItem, data -> {});
 	}
 
+	public void recoverButtonClick(ActionEvent actionEvent) {
+
+	}
 }
