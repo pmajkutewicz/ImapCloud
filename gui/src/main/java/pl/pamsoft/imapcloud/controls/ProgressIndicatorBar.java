8bd891623a1672c2fc25180aef624688d904b2f8
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2016-10-02 21:16:11 +0200

    Checkstyle fix

--- gui/src/main/java/pl/pamsoft/imapcloud/controls/ProgressIndicatorBar.java
+++ gui/src/main/java/pl/pamsoft/imapcloud/controls/ProgressIndicatorBar.java
@@ -35,7 +35,7 @@ public class ProgressIndicatorBar extends StackPane {
 	}
 
 	// synchronizes the progress indicated with the work done.
-	private final void syncProgress() {
+	private void syncProgress() {
 		text.setFill(Color.BLACK);
 		bar.setStyle("-fx-accent: lightblue;");
 		if (workDone == null || totalWork == 0) {
