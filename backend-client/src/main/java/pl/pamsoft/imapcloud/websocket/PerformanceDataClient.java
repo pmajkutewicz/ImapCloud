40b9729680c93e4d20bd5fbb9ed883c7df1c3662
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2016-10-08 16:44:35 +0200

    CPD enabled

--- backend-client/src/main/java/pl/pamsoft/imapcloud/websocket/PerformanceDataClient.java
+++ backend-client/src/main/java/pl/pamsoft/imapcloud/websocket/PerformanceDataClient.java
@@ -1,75 +1,32 @@
 package pl.pamsoft.imapcloud.websocket;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.websocket.ClientEndpointConfig;
-import javax.websocket.CloseReason;
-import javax.websocket.ContainerProvider;
-import javax.websocket.DeploymentException;
-import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.MessageHandler;
 import javax.websocket.Session;
-import javax.websocket.WebSocketContainer;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
-public class PerformanceDataClient extends Endpoint implements MessageHandler.Whole<String> {
+public class PerformanceDataClient extends AbstractWebsocketClient<PerformanceDataEventListener> implements MessageHandler.Whole<String> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(PerformanceDataClient.class);
-	private final String endpoint;
-	private final String username;
-	private final String password;
-	ObjectMapper mapper = new ObjectMapper();
-	private CountDownLatch latch = new CountDownLatch(1);
-	private List<PerformanceDataEventListener> listeners = new ArrayList<>();
-	private Session websocketSession;
 
 	public PerformanceDataClient(String endpoint, String username, String password) {
-		this.endpoint = String.format("ws://%s/performance", endpoint);
-		this.username = username;
-		this.password = password;
-	}
-
-	public void addListener(PerformanceDataEventListener listener) {
-		this.listeners.add(listener);
-	}
-
-	public void removeListener(PerformanceDataEventListener listener) {
-		this.listeners.remove(listener);
-	}
-
-	public void connect() throws IOException, URISyntaxException, InterruptedException, DeploymentException {
-		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-		container.connectToServer(this, ClientEndpointConfig.Builder.create().configurator(new AuthorizationConfigurator(username, password)).build(), new URI(endpoint));
-		getLatch().await();
-	}
-
-	public void disconnect() throws IOException, URISyntaxException, InterruptedException, DeploymentException {
-		if (websocketSession.isOpen()) {
-			websocketSession.close();
-		}
+		super(endpoint, "performance", username, password);
 	}
 
 	@Override
 	public void onOpen(Session session, EndpointConfig config) {
 		System.out.println("Connected to performance data server");
-		this.websocketSession = session;
-		websocketSession.addMessageHandler(this);
-		latch.countDown();
+		super.onOpen(session, config);
 	}
 
 	@Override
 	public void onMessage(String message) {
-		for (PerformanceDataEventListener listener : listeners) {
+		for (PerformanceDataEventListener listener : getListeners()) {
 			try {
-				PerformanceDataEvent eventData = mapper.readValue(message, PerformanceDataEvent.class);
+				PerformanceDataEvent eventData = getMapper().readValue(message, PerformanceDataEvent.class);
 				listener.onEventReceived(eventData);
 			} catch (IOException e) {
 				LOG.warn("Can't deserialize json", e);
@@ -77,26 +34,4 @@ public class PerformanceDataClient extends Endpoint implements MessageHandler.Wh
 		}
 		LOG.info("Message received from server: {}", message);
 	}
-
-	@Override
-	public void onClose(Session session, CloseReason closeReason) {
-		LOG.info("Closing a WebSocket due to {}", closeReason.getReasonPhrase());
-	}
-
-	public CountDownLatch getLatch() {
-		return latch;
-	}
-
-	public void sendMessage(String str) throws IOException {
-		websocketSession.getBasicRemote().sendText(str);
-	}
-
-	public void close() {
-		try {
-			websocketSession.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 }
--- backend-client/src/main/java/pl/pamsoft/imapcloud/websocket/TaskProgressClient.java
+++ backend-client/src/main/java/pl/pamsoft/imapcloud/websocket/TaskProgressClient.java
@@ -1,99 +1,35 @@
 package pl.pamsoft.imapcloud.websocket;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.websocket.ClientEndpointConfig;
-import javax.websocket.CloseReason;
-import javax.websocket.ContainerProvider;
-import javax.websocket.DeploymentException;
-import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.MessageHandler;
 import javax.websocket.Session;
-import javax.websocket.WebSocketContainer;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
-public class TaskProgressClient extends Endpoint implements MessageHandler.Whole<String> {
+public class TaskProgressClient extends AbstractWebsocketClient<TaskProgressEventListener> implements MessageHandler.Whole<String> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TaskProgressClient.class);
-	private final String endpoint;
-	private final String username;
-	private final String password;
-	ObjectMapper mapper = new ObjectMapper();
-	private CountDownLatch latch = new CountDownLatch(1);
-	private List<TaskProgressEventListener> listeners = new ArrayList<>();
-	private Session websocketSession;
 
 	public TaskProgressClient(String endpoint, String username, String password) {
-		this.endpoint = String.format("ws://%s/tasks", endpoint);
-		this.username = username;
-		this.password = password;
-	}
-
-	public void addListener(TaskProgressEventListener listener) {
-		this.listeners.add(listener);
-	}
-
-	public void removeListener(TaskProgressEventListener listener) {
-		this.listeners.remove(listener);
-	}
-
-	public void connect() throws IOException, URISyntaxException, InterruptedException, DeploymentException {
-		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-		container.connectToServer(this, ClientEndpointConfig.Builder.create().configurator(new AuthorizationConfigurator(username, password)).build(), new URI(endpoint));
-		getLatch().await();
-	}
-
-	public void disconnect() throws IOException, URISyntaxException, InterruptedException, DeploymentException {
-		if (websocketSession.isOpen()) {
-			websocketSession.close();
-		}
+		super(endpoint, "tasks", username, password);
 	}
 
 	public void onOpen(Session session, EndpointConfig config) {
-		System.out.println("Connected to task progress server");
-		this.websocketSession = session;
-		websocketSession.addMessageHandler(this);
-		latch.countDown();
+		LOG.debug("Connected to task progress server");
+		super.onOpen(session, config);
 	}
 
 	public void onMessage(String message) {
-		for (TaskProgressEventListener listener : listeners) {
+		for (TaskProgressEventListener listener : getListeners()) {
 			try {
-				TaskProgressEvent eventData = mapper.readValue(message, TaskProgressEvent.class);
+				TaskProgressEvent eventData = getMapper().readValue(message, TaskProgressEvent.class);
 				listener.onEventReceived(eventData);
 			} catch (IOException e) {
 				LOG.warn("Can't deserialize json", e);
 			}
 		}
 		LOG.info("Message received from server: {}", message);
-	}
-
-	@Override
-	public void onClose(Session session, CloseReason closeReason) {
-		LOG.info("Closing a WebSocket due to {}", closeReason.getReasonPhrase());
-	}
-
-	public CountDownLatch getLatch() {
-		return latch;
-	}
-
-	public void sendMessage(String str) throws IOException {
-		websocketSession.getBasicRemote().sendText(str);
-	}
-
-	public void close() {
-		try {
-			websocketSession.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
