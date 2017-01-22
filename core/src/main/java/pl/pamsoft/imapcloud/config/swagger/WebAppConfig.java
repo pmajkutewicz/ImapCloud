1f1118ee5cb7d8f5d7633ac474fae5ad8f0a134d
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2017-01-22 20:36:40 +0100

    Added ff4j web, /ff4j-web-console not fully working

--- core/src/main/java/pl/pamsoft/imapcloud/config/swagger/WebAppConfig.java
+++ core/src/main/java/pl/pamsoft/imapcloud/config/swagger/WebAppConfig.java
@@ -11,12 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-		"classpath:/META-INF/resources/", "classpath:/resources/", "classpath:/static/", "classpath:/public/"};
+		"classpath:/META-INF/resources", "classpath:/resources", "classpath:/static", "classpath:/public"};
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("/webjars/*").addResourceLocations("classpath:/META-INF/resources/webjars/");
 		if (!registry.hasMappingForPattern("/**")) {
 			registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
 		}
