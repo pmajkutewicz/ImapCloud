f9362f3926ee753e5a1134728c117ca8180554d2
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2017-12-13 19:56:17 +0100

    Jamon beans fix

--- core/src/main/java/pl/pamsoft/imapcloud/config/jmx/Jamon.java
+++ core/src/main/java/pl/pamsoft/imapcloud/config/jmx/Jamon.java
@@ -1,10 +1,10 @@
 package pl.pamsoft.imapcloud.config.jmx;
 
-
 import com.jamonapi.jmx.JmxUtils;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Configuration
 public class Jamon {
@@ -12,5 +12,10 @@ public class Jamon {
 	@PostConstruct
 	public void init() {
 		JmxUtils.registerMbeans();
+	}
+
+	@PreDestroy
+	public void destroy() {
+		JmxUtils.unregisterMbeans();
 	}
 }
